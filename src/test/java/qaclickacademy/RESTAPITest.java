17180235fc994c679b0f2bab2e282d2b3a8ce5ba
Marian <negoescumariandorel@gmail.com>
2023-11-25 15:24:06 +0200

    American guy commit

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -11,6 +11,15 @@ public class RESTAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
+		// sadsfsf
+		// dfdfdf
+		// dfgfg
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
+
+		
 		
 		
 	}
